aec648fc52b6ed70357b4cc1924c682c052fe93f
softwaree40 <softwaree40@gmail.com>
2021-01-02 01:23:50 -0500

    creating new searchEngine

---
+++ src/components/DisplayInformation.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+
+const DisplayInformation = ()=>{
+
+
+ return (<div>Display Information!</div>)
+
+
+
+}
+
+export default DisplayInformation
---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+
+
+class SearchBar extends React.Component{
+
+render(){
+
+ return(<div>
+
+       <h1>Search Bar!</h1>
+
+ </div>
+ )
+
+}
+
+}
+
+export default SearchBar 
